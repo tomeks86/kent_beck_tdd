017f1a3a497290fbdc18b0b0ff4c5fbfabb3341e
Tomasz Seidler <tomasz.z.seidler@gmail.com>
2018-04-07 10:42:33 +0200

    franc implementation with copy-paste dollar's implementation

---
+++ src/main/java/edu/tseidler/Franc.java
@@ -0,0 +1,19 @@
+package edu.tseidler;
+
+public class Franc {
+    int amount;
+
+    public Franc(int amount) {
+        this.amount = amount;
+    }
+
+    public Franc times(int multiplier) {
+        return new Franc(amount * multiplier);
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        Franc franc = (Franc) object;
+        return franc.amount == amount;
+    }
+}
