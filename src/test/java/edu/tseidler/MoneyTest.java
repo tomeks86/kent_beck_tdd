6e7afadc7f5a79ce349e6e517ad273bf6adf1ece
Tomasz Seidler <tomasz.z.seidler@gmail.com>
2018-04-07 10:38:07 +0200

    equals first implementation

--- src/test/java/edu/tseidler/MoneyTest.java
+++ src/test/java/edu/tseidler/MoneyTest.java
@@ -2,7 +2,9 @@ package edu.tseidler;
 
 import org.junit.Test;
 
+import static junit.framework.TestCase.assertTrue;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 
 public class MoneyTest {
 
@@ -13,6 +15,12 @@ public class MoneyTest {
         assertEquals(10, product.amount);
         product = five.times(3);
         assertEquals(15, product.amount);
+    }
+
+    @Test
+    public void testEquality() {
+        assertTrue(new Dollar(5).equals(new Dollar(5)));
+        assertFalse(new Dollar(5).equals(new Dollar(6)));
     }
 
 }
